0cab9bd067120341bd3468738d98f6b3b2616059
Julie Boisnard <contact@julie-boisnard.fr>
2021-12-17 10:24:49 +0100

    Add category on taskFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 
+use App\Entity\Category;
 use App\Entity\Task;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,11 +12,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; $i++) {
+        $category = new Category();
+        $category->setName('Boring');
+        $manager->persist($category);
+        $manager->flush();
+        for ($i=0; $i<10; $i++) {
             $task = new Task();
             $task->setTitle('Task number ' . $i);
-            $task->setDescription('Blablabla');
+            $task->setDescription('Task description');
             $task->setDone(false);
+
+            $task->setCategory($category);
+
             $manager->persist($task);
         }
 
